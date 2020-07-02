8991b282eee8ff66eca398b8b0a45d97c425edcd
Islam Menglibay <iskomenlibay@gmail.com>
2020-07-02 19:51:21 +0600

    Initial Commit

---
+++ Daily Regime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Daily Regime
+//
+//  Created by Ислам on 7/2/20.
+//  Copyright © 2020 Ислам. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
